08133cf26a6e5c47f8f714399896d407fe0faf24
roacv <roacv@hotmail.com>
2022-12-07 19:06:06 +0000

    Version 1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let pin = ["corazon.png", "diamante.png", "pica.png", "trebol.png"];
+  let rannum = num[Math.floor(Math.random() * num.length)];
+  let ranpin = pin[Math.floor(Math.random() * pin.length)];
+  console.log(rannum);
+  console.log(ranpin);
+  numero.innerHTML = rannum;
+  document.getElementById("imgtop").src = "../src/assets/img/" + ranpin;
+  document.getElementById("imgbot").src = "../src/assets/img/" + ranpin;
+  console.log(numero);
+  // console.log("Hello Rigo from the console!");
 };
